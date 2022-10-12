7db9be3404da340c3179ffc4a7f28ffdcd0bb706
kaustubh87 <kaustubh87@gmail.com>
2022-10-12 07:12:27 -0400

    Added regexp for categories

--- backend/helper/jwt.js
+++ backend/helper/jwt.js
@@ -12,6 +12,10 @@ function authJwt() {
         url: /\/api\/v1\/products(.*)/,
         methods: ["GET", "OPTIONS"],
       },
+      {
+        url: /\/api\/v1\/categories(.*)/,
+        methods: ["GET", "OPTIONS"],
+      },
       `${api}/users/login/`,
       `${api}/users/login/`,
     ],
