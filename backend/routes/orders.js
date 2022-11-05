95169ef4ad4efa9a7a438c5de6b2236e983a2c62
kaustubh87 <kaustubh87@gmail.com>
2022-11-05 13:03:26 -0400

    Populating order, orderItems, product and category

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -4,11 +4,27 @@ const router = express.Router();
 const { OrderItem } = require("../models/order-item.js");
 
 router.get("/", async (req, res) => {
-  const orderList = await Order.find();
+  const orderList = await Order.find()
+    .populate("user", "name")
+    .sort({ dateOrdered: -1 }); //ordered from newest to oldest
   if (!orderList) {
     res.status(500).json({ success: false });
   }
   res.send(orderList);
+});
+
+router.get("/:id", async (req, res) => {
+  const order = await Order.findById(req.params.id)
+    .populate("user", "name")
+    .populate({
+      path: "orderItems",
+      populate: { path: "product", populate: "category" },
+    });
+
+  if (!order) {
+    res.status(500).json({ success: false });
+  }
+  res.send(order);
 });
 
 router.post("/", async (req, res) => {
