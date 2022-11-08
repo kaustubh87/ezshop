76335d64be8c801a77681e41bda2b8337e95ff85
kaustubh87 <kaustubh87@gmail.com>
2022-11-07 21:24:28 -0500

    Updated the logic to calculate totalPrice

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -42,6 +42,20 @@ router.post("/", async (req, res) => {
 
   const orderItemsIdResolved = await orderItemIds;
 
+  const totalPrices = await Promise.all(
+    orderItemsIdResolved.map(async (orderItemId) => {
+      const orderItem = await OrderItem.findById(orderItemId).populate(
+        "product",
+        "price"
+      );
+      const totalPrice = orderItem.product.price * orderItem.quantity;
+      return totalPrice;
+    })
+  );
+
+  const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
+  console.log(totalPrice);
+
   let order = new Order({
     orderItems: orderItemsIdResolved,
     shippingAddress1: req.body.shippingAddress1,
@@ -51,7 +65,7 @@ router.post("/", async (req, res) => {
     country: req.body.country,
     phone: req.body.phone,
     status: req.body.status,
-    totalPrice: req.body.totalPrice,
+    totalPrice: totalPrice,
     user: req.body.user,
   });
 
